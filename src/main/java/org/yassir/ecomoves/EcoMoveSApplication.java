2282fb5147eb29fb6f2f55d2c92520845301f008
yassir5236 <yassiraitlahmadi@gmail.com>
2025-02-28 16:55:10 +0000

    fisrt commit

---
+++ src/main/java/org/yassir/ecomoves/EcoMoveSApplication.java
@@ -0,0 +1,13 @@
+package org.yassir.ecomoves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMoveSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoMoveSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yassir/ecomoves/EcoMoveSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yassir.ecomoves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMoveSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
